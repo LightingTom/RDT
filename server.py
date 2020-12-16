4d13b037d0055c038f731929b3018e4865fc1fde
LightingTom <1452442064@qq.com>
2020-12-16 23:24:51 +0800

    Add server and client

---
+++ server.py
@@ -0,0 +1,30 @@
+# import rdt
+# check_sum = 0x0006+rdt.get_sum(b'hello')
+# print(hex(check_sum))
+# hex_chk_sum = hex(check_sum)[2:]
+# overflow = 0
+# if len(hex_chk_sum) > 4:
+#     overflow = int(hex_chk_sum[:len(hex_chk_sum) - 4], 16)
+# check_sum += overflow
+# print(hex(check_sum)[-4:])
+
+# a = bytes.fromhex('12345678')
+# for i in range(len(a)):
+#     print(hex(a[i]))
+
+# import rdt
+# data = b'hello'
+# packet = rdt.RDTPacket(False,False,False,1,0,len(data),data)
+# encoded = packet.encode()
+# c = rdt.decode(encoded)
+# print(c.payload)
+
+import rdt
+
+s = rdt.RDTSocket()
+s.bind(('127.0.0.1', 8080))
+while True:
+    conn, addr = s.accept()
+    print(conn.recv(10000))
+    conn.send(b'hello,too')
+    print('waiting')
